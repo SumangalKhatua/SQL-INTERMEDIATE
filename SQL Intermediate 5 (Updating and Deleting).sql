6c467545c8cb88627b0008528e1449e5b92b3c35
Sumangal khatua <117670408+SumangalKhatua@users.noreply.github.com>
2023-03-12 16:31:02 +0530

    Add files via upload

---
+++ SQL Intermediate 5 (Updating and Deleting).sql
@@ -0,0 +1,19 @@
+ --updating and deleting data
+
+ --select * from EmployeeDemographics
+
+ --select * from EmployeeDemographics
+ --where Age is null
+
+
+ --update EmployeeDemographics
+ --set EmployeeID = 1012,Age = 31,Gender = 'Female'
+ --where FirstName='holly' And LastName = 'Flax'
+
+ --select * from
+ --EmployeeDemographics
+ --where age is null
+
+ --delete  from
+ --EmployeeDemographics
+ --where age is null
